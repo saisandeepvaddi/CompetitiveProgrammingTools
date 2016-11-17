1b03f60741b781c8d85be4685adbbf0f5f1238b4
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2016-11-16 23:53:20 -0600

    GCD updated in Utils

--- src/Utils/Utils.java
+++ src/Utils/Utils.java
@@ -4,9 +4,11 @@ package Utils;
  * About the code :
  */
 public class Utils {
-    public static int GCD(int a, int b){
-        while(b>0){
-            int tmp = a%b;
+    /*
+    * Usage: Utils.GCD(a, b)*/
+    public static int GCD(int a, int b) {
+        while (b > 0) {
+            int tmp = a % b;
             a = b;
             b = tmp;
         }
